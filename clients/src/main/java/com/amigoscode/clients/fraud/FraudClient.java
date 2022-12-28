69fe8d117b5d2edb0d927b30feae4b0c7ac28c90
ManognaVulchi <madhavi.manogna4@gmail.com>
2022-12-28 16:06:37 -0600

    Refactoring Microservices for k8s

--- clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
+++ clients/src/main/java/com/amigoscode/clients/fraud/FraudClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value="fraud")
+@FeignClient(name="fraud",
+             url = "${clients.fraud.url}")
 public interface FraudClient {
     @GetMapping(path = "api/v1/fraud-check/{customerId}")
     FraudCheckResponse isFraudster(
